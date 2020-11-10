0231df7e73bb2823d2475bcd6ba71e83d943c9fd
hasan-py <hasan29351@gmail.com>
2020-11-10 08:58:35 +0600

    Initial Commit

---
+++ precache-manifest.aa7895c74687ae46c6167be296f1d74d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018f42a7bd2eb6e954422f3e8d0dff7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ec780d9d5ddf3503389",
+    "url": "/static/css/main.ceb30d92.chunk.css"
+  },
+  {
+    "revision": "47abf9221ab0ace12706",
+    "url": "/static/js/2.1968185c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1968185c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec780d9d5ddf3503389",
+    "url": "/static/js/main.114e9e6c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ee068e42424fddbc9f2d32c27f981f10",
+    "url": "/static/media/Lemon Milk Pro Regular.ee068e42.otf"
+  },
+  {
+    "revision": "9c780ef6804067a07a22f40bbdabad5e",
+    "url": "/static/media/Lemon Milk Pro Ultra Light.9c780ef6.otf"
+  }
+]);
